452771d34e66791b546e1ea8f5d79243c0336cc4
Brian <tine0410@mylaurier.ca>
2019-11-23 12:24:22 -0500

    changed temp messages

--- app/src/main/java/com/example/weatherwardrobe/MainActivity.java
+++ app/src/main/java/com/example/weatherwardrobe/MainActivity.java
@@ -189,9 +189,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String a) {
             imageView.setImageBitmap(picture);
-            current_temp.setText(currentTemp + "C\u00b0");
-            min_temp.setText(minTemp + "C\u00b0");
-            max_temp.setText(maxTemp + "C\u00b0");
+            current_temp.setText("Current Temp: " + currentTemp + "C\u00b0");
+            min_temp.setText("Minimum Temp: " + minTemp + "C\u00b0");
+            max_temp.setText("Maximum Temp: " + maxTemp + "C\u00b0");
         }
     }
 }
