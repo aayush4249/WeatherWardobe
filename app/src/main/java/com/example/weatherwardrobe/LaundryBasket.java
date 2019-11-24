b0b87dce6516ae8b9e9695543a68e5f3b309547f
Brian <tine0410@mylaurier.ca>
2019-11-24 14:12:29 -0500

    clean button

--- app/src/main/java/com/example/weatherwardrobe/LaundryBasket.java
+++ app/src/main/java/com/example/weatherwardrobe/LaundryBasket.java
@@ -19,6 +19,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
