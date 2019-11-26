b8f60e97d98e59317698a0156f52c3a84437acf7
Aayush S <aayush4249@gmail.com>
2019-11-26 14:29:42 -0500

    GeoLocating Works

--- app/src/main/java/com/example/weatherwardrobe/AccountActivity.java
+++ app/src/main/java/com/example/weatherwardrobe/AccountActivity.java
@@ -7,11 +7,15 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -32,14 +36,15 @@ import java.security.Permission;
 import java.util.List;
 import java.util.Locale;
 
-public class AccountActivity extends AppCompatActivity {
-    private boolean permissionGranted = false;
+public class AccountActivity extends AppCompatActivity implements LocationListener {
+    boolean permissionGranted = false;
     Location current;
     TextView user_name_label;
     TextView location_label;
     TextView user_name_value;
     TextView location_value;
     ImageView user_image;
+    final int permissionCode = 1234;
     private FusedLocationProviderClient fusedLocationProviderClient;
 
 
@@ -51,7 +56,7 @@ public class AccountActivity extends AppCompatActivity {
         user_name_label = findViewById(R.id.account_name);
         location_label = findViewById(R.id.location_Label);
         user_name_value = findViewById(R.id.name_value);
-        location_value = findViewById(R.id.location_value);
+        location_value = findViewById(R.id.location_Value);
         user_image = findViewById(R.id.user_image);
         final Button setLocation = findViewById(R.id.btn_set_location);
         final Button detectLocation = findViewById(R.id.btn_detect_location);
@@ -69,39 +74,98 @@ public class AccountActivity extends AppCompatActivity {
         detectLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                    get_location();
-                    location_value.setText("");
-                    Geocoder coder = new Geocoder(AccountActivity.this, Locale.getDefault());
-                    StringBuilder stringBuilder = new StringBuilder();
-                    try{
-                        double lat = current.getLatitude();
-                        double lon = current.getLongitude();
-                        List<Address> addresses = coder.getFromLocation(lat,lon,1);
 
-                        int maxLines = addresses.get(0).getMaxAddressLineIndex();
-                        for (int i=0; i<maxLines; i++) {
-                            String addressStr = addresses.get(0).getAddressLine(i);
-                            stringBuilder.append(addressStr);
-                            stringBuilder.append(" ");
-                        }
-
-                        String finalAddress = stringBuilder.toString(); //This is the complete address.
-
-                        location_value.setText(finalAddress); //This will display the final address.
-
-
-                    }catch(IOException e){
-
-                    }catch (NullPointerException e){
-
-                    }
-                }
-                else{
+                LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    Activity#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for Activity#requestPermissions for more details.
                     request_location_permission();
+                    return;
                 }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, (LocationListener) AccountActivity.this);
+
+                //Criteria criteria = new Criteria();
+                //String bestProvider = locationManager.getBestProvider(criteria, true);
+                Location location = getLocation();
+
+                if (location == null) {
+                    Toast.makeText(getApplicationContext(), "GPS signal not found", Toast.LENGTH_SHORT).show();
+                }
+
+                if (location != null) {
+                    onLocationChanged(location);
+                }
+
             }
         });
+    }
+
+    public void onLocationChanged(Location location) {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        Log.i("latitude", "latitude--" + latitude);
+        try {
+            Log.i("latitude", "inside latitude--" + latitude);
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && addresses.size() > 0) {
+                String city = addresses.get(0).getLocality();
+                Log.i("City", " " + city);
+                location_value.setText(city);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    public Location getLocation() {
+        LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        List<String> providers = manager.getProviders(true);
+        Location bestLocation = null;
+
+        for (String provider : providers) {
+
+            @SuppressLint("MissingPermission")
+            Location l = manager.getLastKnownLocation(provider);
+            if (l == null){
+                continue;
+            }
+
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()){
+                bestLocation = l;
+            }
+
+        }
+
+        if (bestLocation == null){
+            return null;
+        }
+        return bestLocation;
     }
 
     //Function that uses alert dialog to build a user input dialog
@@ -135,51 +199,22 @@ public class AccountActivity extends AppCompatActivity {
         builder.show();
     }
 
+
     public void request_location_permission(){
 
-        String[] request = {Manifest.permission.ACCESS_FINE_LOCATION};
-        ActivityCompat.requestPermissions(AccountActivity.this,request,1234);
+        String[] request = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+        ActivityCompat.requestPermissions(AccountActivity.this,request,permissionCode);
 
     }
 
 
     public void onRequestPermission(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 1234){
+        if(requestCode == permissionCode){
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 permissionGranted = true;
             }
         }
     }
 
-
-    //Geo Location
-    public void get_location() {
-
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(AccountActivity.this);
-
-        try {
-            if (permissionGranted) {
-                Task location = fusedLocationProviderClient.getLastLocation();
-                location.addOnCompleteListener(new OnCompleteListener() {
-                    @Override
-                    public void onComplete(@NonNull Task task) {
-                        if (task.isSuccessful()) {
-                            current = (Location) task.getResult();
-                        } else {
-                            Toast toast = Toast.makeText(getApplicationContext(), "Location Service Unavailable", Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-                    }
-                });
-
-
-            }
-        } catch (SecurityException e) {
-            Log.e("Error", "Get Device Location: Security Exception" + e.getMessage());
-
-        }
-
-    }
 }
